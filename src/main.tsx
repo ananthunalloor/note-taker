45ec2f8bcb2f4bcd3b72cb6596e362d2de29d308
ananthunalloor <ananthunalloor@gmail.com>
2024-03-09 13:34:09 +0530

    added mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
+import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { enableMockServiceWorker } from './mocks/browser';
 import { AppEntry } from './pages/app';
 
+import '@mantine/core/styles.css';
+import '@mantine/tiptap/styles.css';
+import '@mantine/notifications/styles.css';
+
 enableMockServiceWorker().then(() =>
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <AppEntry />
+      <MantineProvider>
+        <Notifications />
+        <ModalsProvider>
+          <AppEntry />
+        </ModalsProvider>
+      </MantineProvider>
     </React.StrictMode>
   )
 );
